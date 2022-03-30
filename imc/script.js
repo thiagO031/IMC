1a194f016e36805ff5741dbbd76af3bb5a876501
thiagO031 <thiago.tsantana3@gmail.com>
2022-03-30 18:38:43 -0300

    js em andamento

--- imc/script.js
+++ imc/script.js
@@ -1,12 +1,15 @@
 function analisar(){
+    if(n == 0 || a >= 0 || p >= 0){
+        alert('Preecha todos os campos.')
+    }
+
     let n = document.getElementById('nome').value
     let a = parseInt(document.getElementById('altura').value)
     let p = parseInt(document.getElementById('peso').value)
     let r = document.getElementById('res-conteudo')
 
-    let res = (p / Math.pow(a, 2)).toFixed(2)
+    let res = (p / Math.pow(a, 2))
 
-    // r.innerHTML = `<br> ${n}, o seu IMC é: <br>
-    // ${res}`
     r.innerHTML = `${n}, <br> o seu imc é: <br> ${res}`
+    console.log(res)
 }
