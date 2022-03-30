ea28953d234c9d8f379cd873e5c66c8fe3f2777a
thiagO031 <thiago.tsantana3@gmail.com>
2022-03-30 17:08:33 -0300

    Projeto IMC finalizado

--- imc/script.js
+++ imc/script.js
@@ -2,10 +2,11 @@ function analisar(){
     let n = document.getElementById('nome').value
     let a = parseInt(document.getElementById('altura').value)
     let p = parseInt(document.getElementById('peso').value)
-    let d = document.getElementById('dados')
+    let r = document.getElementById('res-conteudo')
 
-    let res = (p / Math.pow(a, 2))
+    let res = (p / Math.pow(a, 2)).toFixed(2)
 
-    d.innerHTML = `<br> ${n}, o seu IMC é: <br>
-    ${res}`
+    // r.innerHTML = `<br> ${n}, o seu IMC é: <br>
+    // ${res}`
+    r.innerHTML = `${n}, <br> o seu imc é: <br> ${res}`
 }
